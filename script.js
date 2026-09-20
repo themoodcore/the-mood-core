09572bf63cb59ac980f2a93c9eadf83641c7f1fd
themoodcore <themoodcoreuk@gmail.com>
2026-09-20 19:07:59 +0530

    Remove comment for submitCustom function
    Removed comment for the custom request form function.

--- script.js
+++ script.js
@@ -208,7 +208,6 @@ function checkout() {
   window.open(whatsappURL, "_blank");
 }
 
-// 5. Custom Request Form to WhatsApp
 function submitCustom(e) {
   e.preventDefault();
   
@@ -238,6 +237,7 @@ function submitCustom(e) {
   window.open(whatsappURL, "_blank");
   e.target.reset();
 }
+/ 5. Custom Request Form to WhatsApp
 
 // Initialize on Load
 document.addEventListener("DOMContentLoaded", () => {
